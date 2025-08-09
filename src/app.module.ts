875f4fc723a8f8c4f38a52886b6952be52bd33a1
thuanhai2 <thuanhaidz22@gmail.com>
2025-08-09 15:23:12 +0700

    Base-project

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,14 @@
-import { Module } from '@nestjs/common';
+import { MiddlewareConsumer, Module, NestModule } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-
 @Module({
   imports: [],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService
+  ],
 })
-export class AppModule {}
+export class AppModule implements NestModule{
+  configure(consumer: MiddlewareConsumer) {
+    
+  }
+}
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,17 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
+import * as express from 'express';
 
 async function bootstrap() {
+  const PORT = process.env.PORT || 3000;
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+  app.useGlobalPipes(new ValidationPipe());
+  app.use(express.json())
+  app.useGlobalInterceptors();
+  app.use(express.urlencoded({extended:true}));
+  await app.listen(PORT, ()=> {
+    console.log(`Server running on port http://localhost:${PORT}`)
+  });
 }
 bootstrap();
